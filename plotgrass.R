bf0bbf650fc2628683617bce1032c96dc375b9ef
Jose Ramon <joseramon@geografiafisica.org>
2019-05-09 20:23:16 -0400

    added functionality to plotgrass

--- plotgrass.R
+++ plotgrass.R
@@ -1,6 +1,8 @@
 plotgrass <- function(
   gl = NULL, #GRASS GIS layer
   scaledist = 4, #Distance for scale bar (in km)
+  qual = F,
+  legpos = 'right', #'none' to remove
   cols = scale_fill_viridis()  #Palette for ggplot
   #Other options: scale_fill_gradientn(colours = terrain_hcl(3))
   #Other options: scale_fill_viridis(values = rescale(c(1,5,30,100,1000,40775)), direction = -1)
@@ -16,15 +18,19 @@ plotgrass <- function(
       ),
       "SpatialPixelsDataFrame")
   )
+  datacolname <- colnames(rlayer)[which(!colnames(rlayer) %in% c('x','y'))]
+  if(qual) {rlayer[,datacolname] <- as.factor(rlayer[,datacolname])}
   p <- ggplot() +  
     geom_raster(
       data=rlayer,
-      aes_string(x='x', y='y', fill=gl),
-      interpolate = T,
+      aes_string(x = 'x', y = 'y', fill = datacolname),
+      interpolate = ifelse(qual, F, T),
       alpha=1) +
     cols +
     coord_equal() +
     theme_bw() +
+    theme(legend.position = legpos) +
+    labs(title = gl) +
     ggsn::scalebar(
       dist = scaledist,
       st.size=3,
